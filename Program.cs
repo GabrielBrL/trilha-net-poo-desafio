6a23df3dcde56d987631dd4a1b1d457adbc770d2
Gabriel Barros <gabriellourencobarros02@gmail.com>
2023-10-19 07:37:06 -0300

    feat:Adicionando solução e implementando as modificações solicitadas

--- Program.cs
+++ Program.cs
@@ -1,3 +1,11 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Iphone iphone = new Iphone("1199999999");
+Nokia nokia = new Nokia("1199999999");
+
+Console.WriteLine(iphone.Numero);
+Console.WriteLine(nokia.Numero);
+
+nokia.InstalarAplicativo("WhatsApp");
+iphone.InstalarAplicativo("WhatsApp");
